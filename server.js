69b5f2165baf60a17a80ebacc99c93818e6e8ea8
Walter <wromero9556@gmail.com>
2021-02-28 13:46:11 -0300

    First Deploy

--- server.js
+++ server.js
@@ -10,4 +10,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api',router)
 
-app.listen(4000,()=>console.log('App Listening on port 4000'))
+const port = process.env.PORT
+const host = process.env.HOST || '0.0.0.0'
+app.listen(port,host,()=>console.log('App Listening on port 4000'))
