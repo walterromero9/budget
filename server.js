b59b8daa829dc2498ed056e0ea9c196099d45f8e
Walter <wromero9556@gmail.com>
2021-02-28 14:17:12 -0300

    Final deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const router = require('./routers/index')
 
@@ -8,7 +9,15 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api',router)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req,res) =>{
+        res.sendFile(path.join(__dirname+'/client/build/index.html'))
+    })
+}
 
 const port = process.env.PORT
 const host = process.env.HOST || '0.0.0.0'
